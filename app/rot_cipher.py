af12fca9b26c0175f8b563ef375a53702de2b35f
daniel frias <danifriassolsona@gmail.com>
2022-03-11 20:49:21 +0100

    cambiosss

--- app/rot_cipher.py
+++ app/rot_cipher.py
@@ -1,2 +1,15 @@
-def rot13_cipher(msg):
-    pass
+def rot13_cipher(str_in, shift):
+    alpha = "abcdefghijklmnñopqrstuvwxyz"
+    n = len(str_in)
+    str_out = ""
+
+    for i in range(n):
+        c = str_in[i].upper()
+        loc = alpha.find(c)
+        newloc = (loc + shift) % 26
+        str_out += alpha[newloc]
+
+    return str_out
+
+def rot13(msg):
+    return rot13_cipher(msg, 13)
--- tests/test_rot.py
+++ tests/test_rot.py
@@ -1,8 +1,7 @@
-from app.rot_cipher import rot13_cipher
+from app.rot_cipher import rot13
 
 
 class TestClass:
     def test_rot13_cipher(self):
-        assert rot13_cipher("foobar") == "sbbone"
-        assert rot13_cipher("me llamo dani") == "zr yynzb qnav"
-        assert rot13_cipher("juego a futbol") == "whrtb n shgoby" 
+        assert rot13("hola") == "ubyn"
+        assert rot13("adios") == "nqvbf"
