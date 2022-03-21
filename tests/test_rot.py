faf0021f8e59cf4cad5b0abd9713e4d10b06561f
daniel frias <danifriassolsona@gmail.com>
2022-03-21 19:16:56 +0100

    test correctos

--- tests/test_rot.py
+++ tests/test_rot.py
@@ -3,5 +3,5 @@ from app.rot_cipher import rot13
 
 class TestClass:
     def test_rot13_cipher(self):
-        assert rot13("HOLA") == "UBYN"
-        assert rot13("ADIOS") == "NQVBF"
+        assert rot13("HOLA") == "TCXN"
+        assert rot13("ADIOS") == "NPUCG"
